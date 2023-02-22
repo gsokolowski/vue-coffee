79b472de47d265d088e9c5bd1d7c3028ffef8aa8
gsokolowski <gregor.sokolowski@gmail.com>
2023-02-22 20:29:38 +0000

    register components localy

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-Vue.component("plan-picker-component", {
-    template: "#plan-picker-template",
-    data() {
-        return {
-            // plans array is a list of names used by plan-template and component plan is passing this names as props name
-            plans: ["The Single", "The Curious", "The Addict"]
-        }
-      },    
-})
-
-// componnet plan-component
-// contains template #plan-template which is defined in index.html
-Vue.component("plan-component", {
+let PlanComponent = {
   template: "#plan-template",
   props: {
     name: {
@@ -18,8 +6,25 @@ Vue.component("plan-component", {
       required: true,
     },
   },
-});
+};
+
+let PlanPickerComponent = {
+  template: "#plan-picker-template",
+  components: {
+    // component registered localy
+    'plan-component': PlanComponent
+  },
+  data() {
+    return {
+      // plans array is a list of names used by plan-template and component plan is passing this names as props name
+      plans: ["The Single", "The Curious", "The Addict"],
+    };
+  },
+};
 
 new Vue({
   el: "#app",
+  components: {
+    PlanPickerComponent: PlanPickerComponent
+  }
 });
