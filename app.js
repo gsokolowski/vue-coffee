4c48215f5c9b784c59731b179683e14077c4a8de
gsokolowski <gregor.sokolowski@gmail.com>
2023-02-22 20:17:48 +0000

    plan picker component

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+Vue.component("plan-picker-component", {
+    template: "#plan-picker-template",
+    data() {
+        return {
+            // plans array is a list of names used by plan-template and component plan is passing this names as props name
+            plans: ["The Single", "The Curious", "The Addict"]
+        }
+      },    
+})
+
 // componnet plan-component
 // contains template #plan-template which is defined in index.html
-//
 Vue.component("plan-component", {
   template: "#plan-template",
   props: {
@@ -13,8 +22,4 @@ Vue.component("plan-component", {
 
 new Vue({
   el: "#app",
-  data: {
-    // plans array is a list of names used by plan-template and component plan is passing this names as props name
-    plans: ["The Single", "The Curious", "The Addict"],
-  },
 });
