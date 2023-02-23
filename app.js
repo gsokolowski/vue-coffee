55eba16f67305db88c26b6405a8aeff73cd9063f
gsokolowski <gregor.sokolowski@gmail.com>
2023-02-23 19:15:11 +0000

    events added

--- app.js
+++ app.js
@@ -5,6 +5,26 @@ let PlanComponent = {
       type: String,
       required: true,
     },
+    selectedPlan: {
+        type: String
+    },
+  },
+  computed: {
+    isSelected() {
+      // this bit is only for seclectig form input field to know whcih one
+      return this.name === this.selectedPlan
+    }
+  },
+  methods: {
+    select() {
+      this.selected = true;
+      console.log("selected", this.name);
+      this.$emit('select', this.name); // when select on plan is cliced we emit custom event called select 
+      // and pass event data (as payload) this.name of plan so parent knows 
+      // which plan was selected 
+      // then in the parent (index.html /plan-component) we listen fore select event to happen and when it
+      // happens we call the selectPlan method on PlanPickerComponent
+    }
   },
 };
 
@@ -12,19 +32,28 @@ let PlanPickerComponent = {
   template: "#plan-picker-template",
   components: {
     // component registered localy
-    'plan-component': PlanComponent
+    "plan-component": PlanComponent,
   },
   data() {
     return {
       // plans array is a list of names used by plan-template and component plan is passing this names as props name
       plans: ["The Single", "The Curious", "The Addict"],
+      // reset selected plans before user will select new one
+      selectedPlan: null
     };
   },
+  methods: {
+    selectPlan(plan) {
+        // which plan user has selected
+        this.selectedPlan = plan
+        console.log("selected plan: ", this.selectedPlan);
+    }
+  }
 };
 
 new Vue({
   el: "#app",
   components: {
-    PlanPickerComponent: PlanPickerComponent
-  }
+    PlanPickerComponent: PlanPickerComponent,
+  },
 });
